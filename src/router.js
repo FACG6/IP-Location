cb63c72e04c9d0c2398dbd98ddf557e2851bf909
AymanAlQoqa <aalqouqa@gmail.com>
2019-02-06 11:30:07 +0200

    linter is used to solve typing issues relates#15

--- src/router.js
+++ src/router.js
@@ -1,22 +1,21 @@
 const {
-    handleHome,
-    staticHandler,
-    postHandler,
-    notFoundHandler
+  handleHome,
+  staticHandler,
+  postHandler,
+  notFoundHandler,
 } = require('./handler');
 
 const router = (request, response) => {
-    const endpoint = request.url;
-    if (endpoint === '/') {
-        handleHome(request, response,endpoint);
-    } else if (endpoint.includes('/public')) {
-        staticHandler(request, response,endpoint);
-    } else if (endpoint === '/search') {
-        postHandler(request, response);
-    }
-    else {
-        notFoundHandler(request,response);
-    }
-}
+  const endpoint = request.url;
+  if (endpoint === '/') {
+    handleHome(request, response, endpoint);
+  } else if (endpoint.includes('/public')) {
+    staticHandler(request, response, endpoint);
+  } else if (endpoint === '/search') {
+    postHandler(request, response);
+  } else {
+    notFoundHandler(request, response);
+  }
+};
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const router = require('./router.js');
+
 const port = process.env.PORT || 5000;
 const server = http.createServer(router);
 server.listen(port, () => {
-    console.log(`server is running in localhost:${port}`);
-})
+  // eslint-disable-next-line no-console
+  console.log(`server is running in localhost:${port}`);
+});
